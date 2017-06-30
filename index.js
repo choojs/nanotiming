b37dcf160a7cfb61d4f1a4edd44a5d08b94434d9
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2017-06-30 15:25:44 +0200

    disable flag (#5)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var onIdle = require('on-idle')
 var assert = require('assert')
 
-var perf = typeof window !== 'undefined' && window.performance
+var hasWindow = typeof window !== 'undefined'
+var disabled = hasWindow && window.localStorage.DISABLE_NANOTIMING === 'true'
+var perf = hasWindow && window.performance
 var hasPerf = perf && perf.mark
 
 module.exports = nanotiming
@@ -9,7 +11,7 @@ module.exports = nanotiming
 function nanotiming (name) {
   assert.equal(typeof name, 'string', 'nanotiming: name should be type string')
 
-  if (!hasPerf) return noop
+  if (!hasPerf || disabled) return noop
 
   var uuid = (perf.now() * 100).toFixed()
   var startName = 'start-' + uuid + '-' + name
