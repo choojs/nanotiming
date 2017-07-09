761fc2bc5f8136ae20a552fb0d9e8445ef5b60bc
Johnny Hauser <johnnyhauser@gmail.com>
2017-07-09 12:44:05 -0500

    bugfix: localStorage access threw in sandboxed environments, simplified checks for disable

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 var onIdle = require('on-idle')
 var assert = require('assert')
 
-var hasWindow = typeof window !== 'undefined'
-var hasLocalStorage = hasWindow && 'localStorage' in window
-var disabled = hasLocalStorage && window.localStorage.DISABLE_NANOTIMING === 'true'
-var perf = hasWindow && window.performance
-var hasPerf = perf && perf.mark
+var perf
+var disabled = true
+try {
+  perf = window.performance
+  disabled = window.localStorage.DISABLE_NANOTIMING === 'true' || !perf.mark
+} catch (e) { }
 
 module.exports = nanotiming
 
 function nanotiming (name) {
   assert.equal(typeof name, 'string', 'nanotiming: name should be type string')
 
-  if (!hasPerf || disabled) return noop
+  if (disabled) return noop
 
   var uuid = (perf.now() * 100).toFixed()
   var startName = 'start-' + uuid + '-' + name
